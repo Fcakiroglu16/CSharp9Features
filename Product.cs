636291312d42fcd71c0d45355af2e96d667f6017
Fatih Çakıroğlu <f-cakiroglu@outlook.com>
2021-01-16 00:40:59 +0300

    init property lesson

---
+++ Product.cs
@@ -0,0 +1,6 @@
+public  class Product
+{
+
+    public string  Name { get; set; }
+    public int CategoryId { get; init; }
+}
---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Csharp9
+{
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Hello World!");
+
+            //init keyword'ü ile yanlızca object initializer esnasında deger atanabilir. Sonrası için değer atanmaz
+            var product = new Product { Name = "Game", CategoryId = 1 };
+            product.Name="Game2";
+            //product.CategoryId = 10;
+        }
+    }
+}
